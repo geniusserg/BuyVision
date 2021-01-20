44715de299e094a60cf18248063e8671cf0164bf
geniusserg <danilovsergeydmitrievich@gmail.com.com>
2021-01-20 20:58:29 +0300

    Include firebase ML and analytics into project. Create camera intent in main activity.

--- app/src/main/java/com/hse/buyvision/MainActivity.java
+++ app/src/main/java/com/hse/buyvision/MainActivity.java
@@ -2,13 +2,63 @@ package com.hse.buyvision;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Button catch_button, analyze_button;
+    private TextView analyzed_text;
+    private ImageView image_view;
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        catch_button = findViewById(R.id.catch_button);
+        analyzed_text  = findViewById(R.id.analyzed_text);
+        analyze_button = findViewById(R.id.analyze_button);
+        image_view = findViewById(R.id.image_view);
+        catch_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dispatchTakePictureIntent(); //try to take a photo
+            }
+        });
     }
+
+    // React on activities complete
+    // REQUEST_IMAGE_CAPTURE -> save iamge and display it on screen
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            image_view.setImageBitmap(imageBitmap);
+        }
+        // TODO: Logic with ML Kit
+    }
+
+    // Make intent to create photo by camera
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        try {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        } catch (ActivityNotFoundException e) {
+            Toast.makeText(MainActivity.this, "Error: can not take a photo", Toast.LENGTH_LONG);
+        }
+    }
+
+
 }

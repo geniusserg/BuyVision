05081a1dfc653d56564e11ece80e3880aa99f97e
geniusserg <danilovsergeydmitrievich@gmail.com.com>
2021-02-25 19:18:02 +0300

    Refactoring activity: move functions on classes

--- app/src/main/java/com/hse/buyvision/Analyzer.java
+++ app/src/main/java/com/hse/buyvision/Analyzer.java
@@ -1,4 +1,33 @@
 package com.hse.buyvision;
 
 
+import com.google.firebase.ml.vision.text.FirebaseVisionText;
 
+class Analyzer{
+    private static Analyzer instance = null;
+    private Analyzer(){
+    }
+    public static Analyzer getInstance(){
+        if (instance == null){
+            instance = new Analyzer();
+        }
+        return instance;
+    }
+
+    public String parseFirebaseVisionTextBlocks(FirebaseVisionText result){
+        String resultText = "";
+        for (FirebaseVisionText.TextBlock block: result.getTextBlocks()) {
+            String blockText = "";
+            for (FirebaseVisionText.Line line: block.getLines()) {
+                for (FirebaseVisionText.Element element: line.getElements()) {
+                    String elementText = element.getText();
+                    blockText += elementText + " ";
+                }
+            }
+            resultText += blockText + "\n";
+        }
+        System.out.println("TEXT RECOGNITION RESULT");
+        System.out.println(resultText);
+        return resultText;
+    }
+}
--- app/src/main/java/com/hse/buyvision/Preprocessor.java
+++ app/src/main/java/com/hse/buyvision/Preprocessor.java
@@ -5,11 +5,21 @@ import android.graphics.Canvas;
 import android.graphics.ColorMatrix;
 import android.graphics.ColorMatrixColorFilter;
 import android.graphics.Paint;
+import android.os.Environment;
+import android.util.Log;
+import java.io.File;
+import android.provider.MediaStore;
+import androidx.core.content.FileProvider;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Preprocessor {
-    private Bitmap source;
-    public Preprocessor(Bitmap inputBitmap){
-        source = inputBitmap;
+    private static Bitmap source;
+    private static Preprocessor instance = null;
+    private Preprocessor(){
     }
 
     public Bitmap preprocess(){
@@ -18,6 +28,17 @@ public class Preprocessor {
         return source;
     }
 
+    public static Preprocessor getInstance(){
+        if (instance == null){
+            instance = new Preprocessor();
+        }
+        return instance;
+    }
+
+    public static Preprocessor getInstance(Bitmap inputSource){
+        source = inputSource;
+        return getInstance();
+    }
     public Bitmap getSource() {
         return source;
     }
@@ -60,4 +81,19 @@ public class Preprocessor {
     public void catTextBlocks(){
 //cat text block
     }
+
+    public File createImageFile(File storageDir) throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+        Log.println(Log.INFO, "File", "File");
+        return image;
+    }
+
+
 }

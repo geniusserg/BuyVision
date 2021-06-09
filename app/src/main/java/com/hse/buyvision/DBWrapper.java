9f7e0e673b206ef68654739f40b11be5fc1c01e7
Sergey Danilov <sddanilov_1@edu.hse.ru>
2021-06-09 21:22:40 +0300

    Minor fixes #2

--- app/src/main/java/com/hse/buyvision/DBWrapper.java
+++ app/src/main/java/com/hse/buyvision/DBWrapper.java
@@ -4,19 +4,20 @@ import java.io.File;
 import java.util.Date;
 import java.util.ArrayList;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 
 public class DBWrapper{
     private DBHelper dbHelper;
-    private static int counter = 0;
+    private static long counter = 0;
     private ArrayList<ItemModel> itemsArray = new ArrayList<ItemModel>();
-    public DBWrapper(DbHelper dbHelper) {};
+    public DBWrapper(DBHelper dbHelper) { this.dbHelper = dbHelper; };
     public void save(ItemModel item){
         SQLiteDatabase db = dbHelper.getWritableDatabase();
         ContentValues values = new ContentValues();
         values.put(DatabaseEntry.COLUMN_NAME_DATE, item.date.getTime());
         values.put(DatabaseEntry.COLUMN_NAME_TEXT, item.text);
         values.put(DatabaseEntry.COLUMN_NAME_FILE, item.photo.getAbsolutePath());
-        long newRowId = db.insert(TABLE_NAME, null, values);
+        long newRowId = db.insert(DatabaseEntry.TABLE_NAME, null, values);
         counter = newRowId;
     }
     public void loadResults(){
@@ -49,12 +50,12 @@ public class DBWrapper{
     public ItemModel getPrev(){
         if (counter < 0 ){ return null;}
         counter -= 1;
-        return itemsArray.get(counter);
+        return itemsArray.get((int)counter);
     }
     public ItemModel getNext(){
         if (counter > itemsArray.size() ){ return null;}
         counter += 1;
-        return itemsArray.get(counter);
+        return itemsArray.get((int)counter);
     }
 
 }
--- app/src/main/java/com/hse/buyvision/DatabaseEntry.java
+++ app/src/main/java/com/hse/buyvision/DatabaseEntry.java
@@ -2,7 +2,7 @@ package com.hse.buyvision;
 
 import android.provider.BaseColumns;
 
-public static class DatabaseEntry implements BaseColumns {
+public final class DatabaseEntry implements BaseColumns {
     public static final String TABLE_NAME = "SCANS";
     public static final String COLUMN_NAME_DATE = "DATE";
     public static final String COLUMN_NAME_TEXT = "TEXT";
